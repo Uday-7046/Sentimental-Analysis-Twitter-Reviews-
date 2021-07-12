198d07204509c8d57c0c7e256447cfa9a21c5fa9
Uday-7046 <85827280+Uday-7046@users.noreply.github.com>
2021-07-12 15:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model = joblib.load('Twitter_Reviews')
+model = joblib.load('Twitter-Reviews')
 st.title('Twitter Reviews Classifier')
 ip = st.text_input('Enter your Tweet')
 op = model.predict([ip])
